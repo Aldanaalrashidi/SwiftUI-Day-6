a6e27711a22e0c26436749bc1e0200a5b6ca491d
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-08-29 16:02:13 +0300

    Initial Commit

---
+++ SwiftUI Day6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Day6
+//
+//  Created by tahani alrashidi on 8/29/20.
+//  Copyright © 2020 Aldana Al-Rashidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
